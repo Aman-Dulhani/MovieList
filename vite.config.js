243c8d82f4f145a597f7aa22635f398dc99cdbb7
Aman Dulhani <aman.dulhani97@gmail.com>
2024-05-31 16:18:13 +0530

    Vite config file update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	server: {
+		host: "localhost.3ds.com",
+		port: 3000,
+		open: true,
+	},
+	plugins: [react()],
+});
